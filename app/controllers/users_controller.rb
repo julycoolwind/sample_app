1bba127e7633134bc65bb65aae34d7b4df812d0d
林鑫 <julycoolwind@hotmail.com>
2015-07-03 13:38:36 +0800

    Finish 10.3.1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -50,10 +50,6 @@ class UsersController < ApplicationController
     end
 
     # Before filters
-    def signed_in_user
-      store_location
-      redirect_to signin_url, notice: "Please sign in." unless signed_in?
-    end
 
     def correct_user
       @user = User.find(params[:id])
